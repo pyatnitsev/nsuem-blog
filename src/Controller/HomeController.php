c6589cca61f8a127b6e3b893d34a7bbfd2fdab1b
Danil Pyatnitsev <danil@pyatnitsev.ru>
2023-10-16 22:36:07 +0300

    change database to mysql, add articles.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private const RECENT_ARTICLE_COUNT_ON_HOME = 5;
+
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articleRepository->getRecentArticles(self::RECENT_ARTICLE_COUNT_ON_HOME),
         ]);
     }
 }
