2e6641fa549b5874f297dfcba4008d7bea645303
Danil Pyatnitsev <danil@pyatnitsev.ru>
2023-10-17 08:35:39 +0300

    adds frontend, adds translation, adds validation, adds article edit adds bootstrap

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,14 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+            ->add('title', TextType::class, [
+                'label' => 'Название статьи',
+                'required' => true,
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Текст статьи',
+                'required' => true,
+            ])
         ;
     }
 
